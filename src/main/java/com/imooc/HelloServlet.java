cb679e8f7c2de08ffbc58aa1b07c096517bedef5
cy08312 <1242418304@qq.com>
2020-07-24 16:46:06 +0800

    update code

--- src/main/java/com/imooc/HelloServlet.java
+++ src/main/java/com/imooc/HelloServlet.java
@@ -1,5 +1,6 @@
 package com.imooc;
 
+import lombok.extern.slf4j.Slf4j;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -7,13 +8,14 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@Slf4j
 @WebServlet("/hello")
 public class HelloServlet extends HttpServlet {
-
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String name = "我的简易框架";
         req.setAttribute("name", name);
+        log.debug("name is " + name);
         req.getRequestDispatcher("/WEB-INF/jsp/hello.jsp").forward(req, resp);
     }
 }
